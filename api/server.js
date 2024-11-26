7639779d552b10cb014a6330da078c8aca4c8c64
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2024-11-26 10:42:26 +0900

    서버 올리기 위해 수정2

--- api/server.js
+++ api/server.js
@@ -1,18 +1,13 @@
 import express from 'express';
 import mysql from 'mysql2';
 import cors from 'cors';
-import path from 'path';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-  
+
 // MySQL 연결
 const connection = mysql.createConnection({
     host: 'localhost',
